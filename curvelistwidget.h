341d73f283ae90cb0bffd524b165c04d3549ecf1
Christophe Colin <popscolin@gmail.com>
2017-09-02 10:28:15 +0200

    Add curves selection through spin boxes

--- curvelistwidget.h
+++ curvelistwidget.h
@@ -20,24 +20,22 @@ public:
     explicit CurveListWidget(QWidget *parent = 0);
     ~CurveListWidget();
 
-
     void    clear();
     void    addCurve(Curve *curve);
     Curve * getSelectedCurve();
     QList<Curve *> getSelectedCurves();
+    void setCurveRangeMax(void);
 
 private slots:
     void global_type_changed(int type);
     void rowselected(int row, int column);
     void on_clearall_pressed();
-
     void on_displayall_pressed();
-
     void on_displayoff_pressed();
-
-    void on_delete_2_pressed();
-
+    void on_deletecurve_pressed();
+    void on_redraw_pressed();
     void updateshiftvalue();
+
 private:
     Ui::CurveListWidget *ui;
 };
